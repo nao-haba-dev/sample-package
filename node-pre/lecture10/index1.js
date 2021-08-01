0b4f771de56674fb8370e5aafa3a8565ceb4a006
haba@mic9 <haba@mic9.co.jp>
2021-08-01 20:06:03 +0900

    feat

--- node-pre/lecture10/index1.js
+++ node-pre/lecture10/index1.js
@@ -1,15 +1,23 @@
+/**
+ * 非同期処理
+ * resolve 成功
+ * reject 失敗
+ */
 var p1 = new Promise((resolve, reject) => {
-  setTimeout(function() {
+  setTimeout(function () {
     reject("Hello Worls.");
   }, 100);
 });
 
-p1.then((value) => {
-  console.log(`.then() onfullfiled : ${value}`);
-}, (reason) => {
-  console.log(`.then() onrejected  : ${reason}`);
-});
+p1.then(
+  (value) => {
+    console.log(`.then() onfullfiled : ${value}`);
+  },
+  (reason) => {
+    console.log(`.then() onrejected  : ${reason}`);
+  }
+);
 
-p1.catch((reason)=>{
+p1.catch((reason) => {
   console.log(`.catch() onrejected : ${reason}`);
 });
--- node-pre/lecture10/index2.js
+++ node-pre/lecture10/index2.js
@@ -10,8 +10,22 @@ var p2 = new Promise((resolve, reject) => {
   }, Math.random() * 3000);
 });
 
-Promise.all([p1, p2]).then((value) => {
-  console.log(value);
-}, (reason) => {
-  console.log(reasom);
-});
+// 全てのデータが揃ったら実行
+Promise.all([p1, p2]).then(
+  (value) => {
+    console.log(value);
+  },
+  (reason) => {
+    console.log(reasom);
+  }
+);
+
+// 思い処理でタイムアウト判定する場合などで使う
+Promise.race([p1, p2]).then(
+  (value) => {
+    console.log(value);
+  },
+  (reason) => {
+    console.log(reasom);
+  }
+);
